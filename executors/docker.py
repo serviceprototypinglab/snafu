6b7c5f49d377e8ebb4a0143479eb275c09079ab2
lambda <spio@docker>
2017-03-10 14:59:30 +0100

    - basic working implementation of the docker executor - docker file extended with the necessary components (docker.io, requests)

--- executors/docker.py
+++ executors/docker.py
@@ -4,12 +4,16 @@ import requests
 import os
 import configparser
 
-def executecontrol(flaskrequest, tenant):
-	endpoint = None
+container = "jszhaw/snafu"
 
-	# FIXME: construct endpoint by launching a docker container per tenant
-	# FIXME: with some persistent or session-based mapping tenant->port number
-	# FIXME: + volume mapping: functions-tenants/<tenant>
+endpoints = {}
+
+def executecontrol(flaskrequest, tenant):
+	if not tenant in endpoints:
+		portnum = 10000 + len(endpoints) + 1
+		endpoints[tenant] = "http://localhost:{}".format(portnum)
+		os.system("docker run -d -p 127.0.0.1:{}:10000 -v /opt/functions-tenants/{}:/opt/functions-local {}".format(portnum, tenant, container))
+	endpoint = endpoints[tenant]
 
 	headers = {}
 	headers["X-Amz-Date"] = flaskrequest.headers.get("X-Amz-Date")
