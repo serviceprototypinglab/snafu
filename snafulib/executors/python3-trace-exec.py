78da8e2ff35033107249ca8cfc9070363c0850db
Nopx <jollans94@googlemail.com>
2017-10-27 18:54:50 +0200

    Added executor for basic tracing

---
+++ snafulib/executors/python3-trace-exec.py
@@ -0,0 +1,76 @@
+# Snafu: Snake Functions - Python3 Executor (Exec Module)
+
+import sys
+import os
+import time
+import json
+import base64
+import pickle
+
+class Context:
+	def __init__(self):
+		self.SnafuContext = self
+
+	#def __new__(self):
+	#	self.SnafuContext = self
+	#	return self
+
+def trace(frame, event, arg):
+	if not 'tracetime' in globals():
+		global tracetime
+		tracetime = None
+	if event=='call':
+		out_file = open('testfile.txt','a')
+		if tracetime==None:
+			tracetime=time.time()
+		else:
+			helptime = time.time()
+			timedif=int(round((helptime-tracetime)*1000000))
+			out_file.write(' time taken: '+str(timedif)+' µs\n')
+			tracetime = helptime
+		line_no = frame.f_lineno
+		funcname = frame.f_code.co_name
+		filename = frame.f_code.co_filename
+		caller = frame.f_back
+		caller_funcname = caller.f_code.co_name
+		caller_filename = caller.f_code.co_filename
+		out_file.write(event+' to '+str(filename)+'.'+str(funcname)+' by '+caller_filename+'.'+caller_funcname)
+	#print(event+' to '+str(filename)+'.'+str(funcname))
+
+def execute(filename, func, funcargs, envvars):
+	funcargs = json.loads(funcargs)
+	envvars = json.loads(envvars)
+
+	for i, funcarg in enumerate(funcargs):
+		if type(funcarg) == str and funcarg.startswith("pickle:"):
+			sys.modules["lib"] = Context()
+			sys.modules["lib.snafu"] = Context()
+			#funcarg = pickle.loads(base64.b64decode(funcarg.split(":")[1]))
+			# FIXME: SnafuContext as in python2-exec module
+			funcarg = None
+
+	sys.path.append(".")
+	os.chdir(os.path.dirname(filename))
+	mod = __import__(os.path.basename(filename[:-3]))
+	func = getattr(mod, func)
+
+	for envvar in envvars:
+		os.environ[envvar] = envvars[envvar]
+
+	stime = time.time()
+	try:
+		#activate tracing
+		sys.settrace(trace)
+		res = func(*funcargs)
+		#deactivate tracing
+		sys.settrace(None)
+		success = True
+	except Exception as e:
+		res = e
+		success = False
+	dtime = (time.time() - stime) * 1000
+
+	#return dtime, success, res
+	return "{} {} {}".format(dtime, success, "{}".format(res).replace("'", "\""))
+
+print(execute(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]))
