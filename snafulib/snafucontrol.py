bb5fa2598f6ee439c0d1538c91aeea6ee4afaae2
spio <spio@tiguriner>
2018-02-12 22:39:28 +0100

    - bugfix: activate parsers also in daemon

--- snafulib/snafucontrol.py
+++ snafulib/snafucontrol.py
@@ -540,6 +540,7 @@ class SnafuControl:
 		# FIXME: should now provide the choice between lambda, gfunctions and openwhisk depending on modular activation
 
 		self.snafu.configpath = args.settings
+		self.snafu.setupparsers(snafulib.snafu.selectparsers(args.function_parser))
 		self.snafu.activate(args.file, "lambda", ignore=ignore)
 		self.snafu.setuploggers(args.logger)
 		self.snafu.setupexecutors(snafulib.snafu.selectexecutors(args.executor))
